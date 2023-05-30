5f587b912796dae012d4b9704b6621eb1f407a6e
mjcoding0702 <chungmangjie2000@gmail.com>
2023-05-30 12:01:42 +0800

    Added experience fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+function Experience() {
+  return (
+    <>
+      <h3>Student</h3>
+      <h4>AIMST University</h4>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
-    <h1>hehe Chung Mang Jie</h1>
+    <h1>Chung Mang Jie</h1>
     <h2>Software developer</h2>
+    <Experience/>
     </>
   )
 }
